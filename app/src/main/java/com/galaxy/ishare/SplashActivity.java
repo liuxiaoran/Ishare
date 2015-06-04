e9e551271c5901168ec0214f850de16aa51b2aca
hanyu <doqin_yu@163.com>
2015-06-04 22:42:10 +0800

    引导页的添加

--- app/src/main/java/com/galaxy/ishare/SplashActivity.java
+++ app/src/main/java/com/galaxy/ishare/SplashActivity.java
@@ -5,14 +5,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.galaxy.ishare.constant.BroadcastActionConstant;
 import com.galaxy.ishare.login.LoginActivity;
 import com.galaxy.ishare.main.MainActivity;
+
+import java.util.ArrayList;
 
 /**
  * Created by liuxiaoran on 15/4/28.
@@ -21,6 +31,20 @@ public class SplashActivity extends Activity {
 
 
     public static final String TAG = "splashactivity";
+    private ViewPager viewPager;
+
+    /**装分页显示的view的数组*/
+    private ArrayList<View> pageViews;
+    private ImageView imageView;
+
+    /**将小圆点的图片用数组表示*/
+    private ImageView[] imageViews;
+
+    //包裹滑动图片的LinearLayout
+    private ViewGroup viewPics;
+
+    //包裹小圆点的LinearLayout
+    private ViewGroup viewPoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +53,176 @@ public class SplashActivity extends Activity {
 
         if (IShareContext.getInstance().getCurrentUser()==null){
             // 第一次进入系统
+            LayoutInflater inflater = getLayoutInflater();
+            pageViews = new ArrayList<View>();
+            pageViews.add(inflater.inflate(R.layout.viewpager_page1, null));
+            pageViews.add(inflater.inflate(R.layout.viewpager_page2, null));
 
+            //创建imageviews数组，大小是要显示的图片的数量
+            imageViews = new ImageView[pageViews.size()];
+            //从指定的XML文件加载视图
+            viewPics = (ViewGroup) inflater.inflate(R.layout.activity_guide, null);
 
-            Intent intent1 = new Intent(SplashActivity.this, LoginActivity.class);
-            startActivity(intent1);
-            SplashActivity.this.finish();
+            //实例化小圆点的linearLayout和viewpager
+            viewPoints = (ViewGroup) viewPics.findViewById(R.id.viewGroup);
+            viewPager = (ViewPager) viewPics.findViewById(R.id.guidePages);
 
+            //添加小圆点的图片
+            for(int i=0;i<pageViews.size();i++){
+                imageView = new ImageView(SplashActivity.this);
+                //设置小圆点imageview的参数
+                imageView.setLayoutParams(new ViewGroup.LayoutParams(20,20));//创建一个宽高均为20 的布局
+                imageView.setPadding(20, 0, 20, 0);
+                //将小圆点layout添加到数组中
+                imageViews[i] = imageView;
+
+                //默认选中的是第一张图片，此时第一个小圆点是选中状态，其他不是
+                if(i==0){
+                    imageViews[i].setBackgroundResource(R.drawable.dot2_w);
+                }else{
+                    imageViews[i].setBackgroundResource(R.drawable.dot1_w);
+                }
+
+                //将imageviews添加到小圆点视图组
+                viewPoints.addView(imageViews[i]);
+            }
+
+            //显示滑动图片的视图
+            setContentView(viewPics);
+
+            //设置viewpager的适配器和监听事件
+            viewPager.setAdapter(new GuidePageAdapter());
+            viewPager.setOnPageChangeListener(new GuidePageChangeListener());
+//            Intent intent1 = new Intent(SplashActivity.this, LoginActivity.class);
+//            startActivity(intent1);
+//            SplashActivity.this.finish();
 
         }else {
-
-
             Intent intent2 = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent2);
             SplashActivity.this.finish();
+        }
+    }
+    private Button.OnClickListener  Button_OnClickListener = new Button.OnClickListener() {
+        public void onClick(View v) {
+            //设置已经引导
+            setGuided();
 
+            //跳转
+            Intent mIntent = new Intent();
+            mIntent.setClass(SplashActivity.this, LoginActivity.class);
+            SplashActivity.this.startActivity(mIntent);
+            SplashActivity.this.finish();
+        }
+    };
+
+    private static final String SHAREDPREFERENCES_NAME = "my_pref";
+    private static final String KEY_GUIDE_ACTIVITY = "guide_activity";
+    private void setGuided(){
+        SharedPreferences settings = getSharedPreferences(SHAREDPREFERENCES_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(KEY_GUIDE_ACTIVITY, "false");
+        editor.commit();
+    }
+
+
+    class GuidePageAdapter extends PagerAdapter {
+
+        //销毁position位置的界面
+        @Override
+        public void destroyItem(View v, int position, Object arg2) {
+            // TODO Auto-generated method stub
+            ((ViewPager)v).removeView(pageViews.get(position));
+
+        }
+
+        @Override
+        public void finishUpdate(View arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        //获取当前窗体界面数
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return pageViews.size();
+        }
+
+        //初始化position位置的界面
+        @Override
+        public Object instantiateItem(View v, int position) {
+            // TODO Auto-generated method stub
+            ((ViewPager) v).addView(pageViews.get(position));
+
+            // 测试页卡1内的按钮事件
+            if (position == 1) {
+                Button btn = (Button) v.findViewById(R.id.btn_guide);
+                btn.setOnClickListener(Button_OnClickListener);
+            }
+
+            return pageViews.get(position);
+        }
+
+        // 判断是否由对象生成界面
+        @Override
+        public boolean isViewFromObject(View v, Object arg1) {
+            // TODO Auto-generated method stub
+            return v == arg1;
+        }
+
+
+
+        @Override
+        public void startUpdate(View arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            // TODO Auto-generated method stub
+            return super.getItemPosition(object);
+        }
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public Parcelable saveState() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }
+
+
+    class GuidePageChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            // TODO Auto-generated method stub
+            for(int i=0;i<imageViews.length;i++){
+                imageViews[position].setBackgroundResource(R.drawable.dot1_w);
+                //不是当前选中的page，其小圆点设置为未选中的状态
+                if(position !=i){
+                    imageViews[i].setBackgroundResource(R.drawable.dot2_w);
+                }
+            }
 
         }
     }
